5d8dd7bef4036b52886a64fa8285050ef8ab40dc
stenOskar <stenhalse@gmail.com>
2025-05-04 17:09:45 +0200

    Create item_filters.py
    Class that specify what to filter out.

---
+++ item_filters.py
@@ -0,0 +1,20 @@
+"""
+Item filters for recommendations.
+"""
+
+
+def get_stop_list():
+    """Return a set of items that should be excluded from recommendations"""
+    return {
+        "bærepose",
+        "pose",
+        "plastpose",
+        "handlepose",
+    }
+
+
+def should_exclude_item(item_name):
+    """Check if the item name is in the stoplist."""
+    stopList = get_stop_list()
+    # Case-insensitive check
+    return item_name.lower() in [s.lower() for s in stopList]
